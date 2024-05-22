3c9557406dcb31d611e31230e21c7040151ef586
wesley kaihara <wesleykaihara@gmail.com>
2024-05-21 21:24:21 -0300

    Mais detalhes para geração dos nomes

--- src/module/analysis/core/useCase/GetNamesSuggestions/GetNamesSuggestionsUseCase.ts
+++ src/module/analysis/core/useCase/GetNamesSuggestions/GetNamesSuggestionsUseCase.ts
@@ -22,10 +22,18 @@ export class GetNamesSuggestions {
               description:
                 'Descreve alguns motivos de se utilizar o nome criado, por que ele foi gerado dessa forma, quais impactos ele pode causar e significado caso seja gerado em ingles',
             },
+            keyWords: {
+              type: 'array',
+              description:
+                'Apresentar uma lista com pelo menos 8 itens de palavras chaves para a ideia, esta deve apresentar palavras que representam a startup de forma clara e agreguem valor para o empreender e seus negócio.',
+              items: {
+                type: 'string',
+                description:
+                  'Deve ser gerada uma palavra chave que agregue valor ao negócio e represente de forma clara o que deseja ser resolvido',
+              },
+            },
           },
         },
-        minItems: 5,
-        maxItems: 5,
       },
     },
     required: ['data'],
@@ -44,23 +52,25 @@ export class GetNamesSuggestions {
         },
         {
           role: 'user',
-          content: `Preciso de ajuda para criar algumas sugestões de nomes para uma Startup. Também preciso saber o motivo de se utilizar o nome gerado (Exemplo: Este nome tem uma definição clara do produto e permite que seja facilmente lembrada). A ideia é ${projectDescription}.`,
+          content: `Preciso de ajuda para criar uma lista com sugestões de nomes para a Startup. Também preciso saber o motivo de se utilizar o nome gerado (Exemplo: Este nome tem uma definição clara do produto e permite que seja facilmente lembrada). A ideia é ${projectDescription}.`,
         },
       ],
-      functions: [{ name: 'get_name_suggestions', parameters: this.schema }],
+      functions: [
+        {
+          name: 'get_name_suggestions',
+          description:
+            'Apresentar uma lista com pelo menos 5 sugestões de nomes para a startup, quero nomes que sejam significativos e que representem o que a empresa irá realizar. Também deve ser apresentado o motivo de ter escolhido o nome, por exemplo, representar de forma clara os problemas que a startup resolve. Nome deve ser simples, fácil de se comunicar e não tenha duplo sentido. No retorna também deve haver uma lista com pelo menos 10 palavras chaves para cada geração que representam de forma clara os valores da ideia, quais dores pretende resolver e palavras que chamem a atenção de possíveis investidores',
+          parameters: this.schema,
+        },
+      ],
       function_call: { name: 'get_name_suggestions' },
-      temperature: 1,
+      temperature: 0.9,
     });
     return JSON.parse(analysis.choices[0].message.function_call.arguments).data;
   }
 
   async execute(projectDescription: string): Promise<any> {
-    const namesSuggestions: any[] = [];
-    while (namesSuggestions.length < 3) {
-      const names = await this.getNamesSuggestions(projectDescription);
-      namesSuggestions.push(...names);
-    }
-
+    const namesSuggestions = await this.getNamesSuggestions(projectDescription);
     return namesSuggestions;
   }
 }
